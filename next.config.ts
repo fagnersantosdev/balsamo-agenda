435f7779737336fe7cf2eef84317c11c27d5cb7d
FagnerDev <fagnerpraxedes2@gmail.com>
2026-05-30 21:45:06 -0300

    Fix: Adicionando declaração de tipos para o next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development' // ativa apenas em produção
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -13,6 +15,7 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000', 'balsamo-massoterapia.vercel.app'],
     },
   },
+  turbopack: {},
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA(nextConfig);
